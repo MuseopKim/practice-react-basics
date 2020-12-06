94eab19ba1937496472a31e36895cb7c5f4d6045
ragdoll <museopkim0214@gmail.com>
2020-12-06 22:48:48 +0900

    feat: Add Presenter skeleton codes of each components

--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -1,1 +1,25 @@
-export default () => "SearchPresenter";
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const SearchPresenter = ({
+  movieResults,
+  tvResults,
+  airingToday,
+  loading,
+  searchTerm,
+  handleSubmit,
+  error
+}) => null;
+
+SearchPresenter.propTypes = {
+  movieResults: PropTypes.array,
+  tvResults: PropTypes.array,
+  airingToday: PropTypes.array,
+  error: PropTypes.string,
+  searchTerm: PropTypes.string,
+  loading: PropTypes.bool.isRequired,
+  handleSubmit: PropTypes.func.isRequired
+};
+
+export default SearchPresenter;
